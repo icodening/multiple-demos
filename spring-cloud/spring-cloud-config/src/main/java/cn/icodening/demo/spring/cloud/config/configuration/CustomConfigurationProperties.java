4c9fd73d8c7e158a835e54805455c54e14c1d14d
icodening <cleverbn96@gmail.com>
2022-06-24 23:24:03 +0800

    init spring cloud dynamic config demo

---
+++ spring-cloud/spring-cloud-config/src/main/java/cn/icodening/demo/spring/cloud/config/configuration/CustomConfigurationProperties.java
@@ -0,0 +1,26 @@
+package cn.icodening.demo.spring.cloud.config.configuration;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * @author icodening
+ * @date 2022.06.24
+ */
+@ConfigurationProperties(prefix = "custom.config")
+@RefreshScope
+public class CustomConfigurationProperties {
+
+    private List<String> list = Collections.emptyList();
+
+    public List<String> getList() {
+        return list;
+    }
+
+    public void setList(List<String> list) {
+        this.list = list;
+    }
+}
